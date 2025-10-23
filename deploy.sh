a487a1994b36b6c35da8c600769e2f614f63ce98
Kayode Olaniyi <ifedamilola2009@gmail.com>
2025-10-23 14:39:42 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 git pull
-alembic upgrade head
+source /home/ilanaa_soft/usekudibackend/venv/bin/activate
+pip install -r requirements.txt
+sh run_migrations.sh
 sudo systemctl daemon-reload
 sudo systemctl restart fastapi.service
 sudo service nginx restart
