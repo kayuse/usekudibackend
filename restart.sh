d0c4df2329627abc2913eb86885f0375735aec4a
Kayode Olaniyi <ifedamilola2009@gmail.com>
2025-10-23 15:47:33 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -6,3 +6,8 @@ sudo systemctl stop fastapi
 sudo systemctl start fastapi
 sudo systemctl enable fastapi
 sudo systemctl status fastapi
+sudo systemctl daemon-reload
+sudo systemctl enable celery
+sudo systemctl enable celery-beat
+sudo systemctl start celery
+sudo systemctl start celery-beat
