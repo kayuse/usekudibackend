0b4572600dc6b559d847128e7dd3f82b97da6e53
Kayode Olaniyi <ifedamilola2009@gmail.com>
2025-10-23 14:45:05 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git pull
 pip install -r requirements.txt
-sh /home/ilanaa_soft/usekudibackend/run_migrations.sh
+sh /home/ilanaa_soft/usekudibackend/run_migration.sh
 sudo systemctl daemon-reload
 sudo systemctl restart fastapi.service
 sudo service nginx restart
