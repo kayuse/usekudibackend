9b1e3b8efb991f583ec54791e610910978592e23
Kayode Olaniyi <ifedamilola2009@gmail.com>
2025-10-24 21:58:25 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,14 +1,8 @@
 git pull
 pip install -r requirements.txt
 sh run_migration.sh
-sudo systemctl stop fastapi
-sudo systemctl start fastapi
-sudo systemctl stop celery
-sudo systemctl start celery
-sudo systemctl stop celery-beat
-sudo systemctl start celery-beat
-sudo systemctl status fastapi
-sudo systemctl status celery
-sudo systemctl status celery-beat
+sudo systemctl restart fastapi
+sudo systemctl restart celery
+sudo systemctl restart celery-beat
 sudo systemctl daemon-reload
 sudo service nginx restart
